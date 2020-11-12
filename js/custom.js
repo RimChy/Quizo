cd390e819f8f321967330ca97dc65218d2c48ddc
RimChy <58721247+RimChy@users.noreply.github.com>
2020-11-12 21:18:01 +0600

    js files

---
+++ js/custom.js
@@ -0,0 +1,15 @@
+
+$('.percent').percentageLoader({
+    valElement: 'p',
+    strokeWidth: 10,
+    bgColor: '#000000',
+    ringColor: 'red',
+    textColor: '#ffffff',
+    fontSize: '18px',
+    fontWeight: 'bold'
+  });
+ 
+
+
+
+
